f47da03cf10a72935fa9110fcddc389870c93400
madajaju <darren@pulsipher.org>
2020-04-09 21:48:04 -0700

    BOUQ-6 Add Event Interface for a SubSystem

---
+++ src/Server/AEvent.js
@@ -0,0 +1,24 @@
+module.exports = {
+    addHandlers: (socket) => {
+        for (let event in global.handlers) {
+            socket.on(event, function (data) {
+                for (let i in global.handlers[event].handlers) {
+                    let handler = global.handlers[event].handlers[i];
+                    if (typeof handler === 'string') {
+                        if (global.actions[handler]) {
+                            funcHandler.run(global.actions[handler], event, data);
+                        } else {
+                            console.error("Action not found, for event!", handler)
+                        }
+                    } else {
+                        handler(data);
+                    }
+                }
+            });
+        }
+    },
+    emit: (event, data) => {
+        // TODO: Could check if the event has the right signature in the data
+        global.io.emit(event, data);
+    }
+}
